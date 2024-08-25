9975aad0c2e286fc9532744b4abf47a86cdc82bb
K-iv0 <ksemadilema80@gmail.com>
2024-08-25 20:09:25 +0500

    01.02 переменные

---
+++ maiy.py02peremennee.py
@@ -0,0 +1,5 @@
+kolichestvo = 12
+chas = 1.5
+nazvanie_kursa = 'Python'
+vremyaNaOdnoZ = (chas/kolichestvo)
+print('Курс:', nazvanie_kursa, ', всего задач:', kolichestvo,', затрачено часов:',chas,', среднее время выполнения',vremyaNaOdnoZ,'часа.')
